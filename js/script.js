d4f0cade5372e81036440bcc2e11787936b49c72
Kacper Kaczmarzyk <kacper.kaczmarzyk@gmail.com>
2021-12-29 23:18:27 +0100

    Finalize html, css, add js and complete the site 1.0

--- js/script.js
+++ js/script.js
@@ -1,89 +1,82 @@
-// function which detemrines the computer's choice from among the three options.
-
 function computerPlay () {
-    const choices = ["rock", "paper", "scissors"];
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];;
+    const choices = ["sword", "spear", "horse"];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
 
-// function which asks the player to choose an option, runs the computerPlay
-// function and compares the choices to determine the winner. It returns an ""
-// string if there was a tie and the name of the winner in a string otherwise.
-
-function playRound() {
-
-    let playerChoice = "";
-
-    let correctInput = false;
-    while (correctInput == false) {
-
-        playerChoice = prompt(
-                "Choose rock, paper or scissors by entering the name below: ");
-
-        playerChoice === null ? alert("Please enter one of the options.") : 
-                correctInput = checkPlayerChoice(playerChoice);
-    }
-
+function playGame () {
+    let playerChoice = this.dataset.icon;
     let computerChoice = computerPlay();
-    let result = "";
+    
+    determineResult(playerChoice, computerChoice);
+}
+
+function determineResult (playerChoice, computerChoice) {
+    const resultParagraph = document.querySelector(".result");
 
     if (playerChoice === computerChoice) {
-        console.log("It's a tie, no one wins!");
-    } else if (playerChoice === "rock" && computerChoice !== "paper") {
-        console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
-        result = "player";
-    } else if (playerChoice === "paper" && computerChoice !== "scissors") {
-        console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
-        result = "player";
-    } else if (playerChoice === "scissors" && computerChoice !== "rock") {
-        console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
-        result = "player";
+        resultParagraph.textContent = "It's a tie, no one wins!";
+    } else if (playerChoice === "sword" && computerChoice !== "horse") {
+        resultParagraph.textContent = `You win this round! ${playerChoice} beats ${computerChoice}.`;
+        playerScore += 1;
+    } else if (playerChoice === "spear" && computerChoice !== "sword") {
+        resultParagraph.textContent = `You win this round! ${playerChoice} beats ${computerChoice}.`;
+        playerScore += 1;
+    } else if (playerChoice === "horse" && computerChoice !== "spear") {
+        resultParagraph.textContent = `You win this round! ${playerChoice} beats ${computerChoice}.`;
+        playerScore += 1;
     } else {
-        console.log(`You lose! ${computerChoice} beats ${playerChoice}.`);
-        result = "computer";
+        resultParagraph.textContent = `You lose this round! ${computerChoice} beats ${playerChoice}.`;
+        computerScore += 1;
     }
-    
-    return result;
+
+    updateScore();
+    checkScore();
 }
 
-// creating a function checking whether or not the player entered a
-// correctly-written choice. The function retruns either true or false to change
-// the boolean variable in playRound to exit the while loop.
+function updateScore () {
+    const playerScoreHeader = document.querySelector(".playerScore");
+    const computerScoreHeader = document.querySelector(".computerScore");
 
-function checkPlayerChoice(playerChoice) {
-    playerChoice = playerChoice.toLowerCase();
-    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === 
-            "scissors") {;
-        return true;
-    } else {
-        alert("Please type an option correctly.");
-        return false;
-    }
+    computerScoreHeader.textContent = `Computer: ${computerScore}`;
+    playerScoreHeader.textContent = `Player: ${playerScore}`;
 }
 
-// creating the final game function which runs the playRound function a number
-// of times until the players played 5 rounds which not ended in a tie. It also
-// displays the final message in the console.
+function checkScore () {
+    scoreResultParagraph = document.querySelector(".scoreResult");
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5;) {
-        winner = playRound();
-        i++;
-        winner === "player" ? playerScore++ :
-        winner === "" ? i-- : computerScore++;;
-    }
-
-    if (playerScore > computerScore) {
-        console.log(`The game's over. You win! The final score is ${playerScore}
-                 for you and ${computerScore} for the computer.`);
+    if (playerScore === 10) {
+        scoreResultParagraph.textContent = "You win!"
+    } else if (computerScore === 10) {
+        scoreResultParagraph.textContent = "You lose! Try again by clicking the RESET button."
     } else {
-        console.log(`The game's over. You lose! The final score is 
-                ${playerScore} for you and ${computerScore} for the computer.`);
+        return;
     }
 
+    end();
 }
 
-// game();
+function end () {
+    buttons.forEach((button) => {
+        button.removeEventListener("click", playGame);
+    });
+
+    const resetButton = document.createElement("button");
+    resetButton.classList.add("reset");
+    resetButton.textContent = "RESET";
+    resetButton.addEventListener("click", () => {
+        location.reload();
+    })
+    const scoreDiv = document.querySelector(".score");
+
+    scoreDiv.appendChild(resetButton);
+}
+
+const buttons = document.querySelectorAll("button");
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', playGame
+    )});
+
